cabb70869ca23840e6acf56fa90d1a904c7b8777
prsai0110 <2410030347@klh.edu.in>
2026-02-27 21:28:01 +0530

    Delete nlp directory

--- nlp/embedding_model.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-class EmbeddingModel:
-
-    def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
-
-    def encode(self, text):
-        return self.model.encode(text)
